bbe96e39413490112ab627e45feceedf06a09ba8
Kevin.Bonnet <103254324+kkevin203@users.noreply.github.com>
2022-08-27 10:44:41 +0200

    ajout card

---
+++ src/components/card.js
@@ -0,0 +1,91 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Logo from './../Ressources/Image/img-1.jpg';
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function RecipeReviewCard() {
+  const [expanded, setExpanded] = React.useState(false);
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="éleveur"
+        subheader="01/01/2001"
+      />
+      <CardMedia component="img" height="194" image={Logo} alt="éleveur" />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          Nous vous mettons en contact avec les bouchers de votre régions pour
+          un circuit extra cours .
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+            En choisissant de vous logger en tant quéeleveur vous aurez accés
+            aux boucher de votre régions.
+          </Typography>
+          <Typography paragraph>
+            Ici vous pourrez mettre en vente vos bêtes au prix que vous voulez
+            pour que les boucher vois vos bêtes avec les critère demandé
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
-export default function Home () {
-    return <h1>Home</h1>
-}
+import RecipeReviewCard from '../components/card';
+const MyCard = RecipeReviewCard;
 
+export default function Home() {
+  return <MyCard />; // <h1>Home</h1>
+}
