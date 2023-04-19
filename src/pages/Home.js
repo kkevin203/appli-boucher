f083ecea917618e7f002c6f124ed2791f1a376d8
Kevin.Bonnet <103254324+kkevin203@users.noreply.github.com>
2023-04-19 16:33:09 +0200

    suppression btn setting sur card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,16 +4,17 @@ import RecipeReviewCard from '../components/card';
 const MyCard = RecipeReviewCard;
 
 export default function Home() {
-   return (
-     <>
-     <h1>Home</h1>
-       <Grid container spacing={0}>
-         <Grid item xs={3}></Grid>
-         <Grid item xs={3}>
-           <MyCard
-             avatar="P"
-             title=" Producteur"
-             description="En choisissant de vous logger en tant que producteur vous aurez accés
+  return (
+    <>
+      <h1>Home</h1>
+      <Grid container spacing={0}>
+        <Grid item xs={3}></Grid>
+        <Grid item xs={3}>
+          <MyCard
+            avatar="P"
+            title=" Producteur"
+            img="static/media/img-1.065b9a322135827ff722.jpg"
+            description="En choisissant de vous logger en tant que producteur vous aurez accés
             aux boucher de votre régions."
              description2="Ici vous pourrez mettre en vente vos bêtes au prix que vous voulez pour que les boucher vois vos bêtes avec les critère demandé. "
            />
